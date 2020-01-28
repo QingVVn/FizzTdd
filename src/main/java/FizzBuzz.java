5febe0628c22b060fe4d6f06daf320ee1488bd22
Jing Wang <wangjingmln@163.com>
2020-01-28 23:21:33 +0800

    Refactor code to extract method and variable and replace

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,6 +2,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class FizzBuzz {
+    private static String FIZZ = "Fizz";
+    private static String BUZZ = "Buzz";
+    private static String WHIZZ = "Whizz";
+    private static int FIZZ_NUMBER = 3;
+    private static int BUZZ_NUMBER = 5;
+    private static int WHIZZ_NUMBER = 7;
+
     public static void main(String[] args) throws IOException {
         System.out.println("Enter Students Number:");
         Scanner sc = new Scanner(System.in);
@@ -15,34 +22,44 @@ public class FizzBuzz {
 
     public static String GetFizzBuzzValue(int number)
     {
-        if(String.valueOf(number).contains("3") && !String.valueOf(number).contains("5"))
-            return "Fizz";
-        if(number%3 == 0 && number%5 == 0 && number%7 == 0 && String.valueOf(number).contains("5") && !String.valueOf(number).contains("7"))
-            return "BuzzWhizz";
-        if(number%3 == 0 && number%7 == 0 && String.valueOf(number).contains("5") && !String.valueOf(number).contains("7"))
-            return "Whizz";
-        if(number%3 == 0 && number%5 == 0 && String.valueOf(number).contains("5") && !String.valueOf(number).contains("7"))
-            return "Buzz";
-        if(number%3 == 0 && number%7 == 0 && String.valueOf(number).contains("7"))
-            return "FizzWhizz";
-        if(number%3 == 0 && String.valueOf(number).contains("7"))
-            return "Fizz";
-        if(number%7 == 0 && String.valueOf(number).contains("7"))
-            return "Whizz";
-        if(number%3 == 0 && number%5 == 0 && number%7 == 0)
-            return "FizzBuzzWhizz";
-        if(number%3 == 0 && number%5 == 0)
-            return "FizzBuzz";
-        if(number%3 == 0 && number%7 == 0)
-            return "FizzWhizz";
-        if(number%5 == 0 && number%7 == 0)
-            return "BuzzWhizz";
-        else if(number%3 == 0)
-            return "Fizz";
-        else if(number%5 == 0)
-            return "Buzz";
-        else if(number%7 == 0)
-            return "Whizz";
+        if(isContained(number, FIZZ_NUMBER) && !isContained(number, BUZZ_NUMBER))
+            return FIZZ;
+        if(isMultiple(number, FIZZ_NUMBER) && isMultiple(number, BUZZ_NUMBER) && isMultiple(number, WHIZZ_NUMBER) && isContained(number, BUZZ_NUMBER) && !isContained(number, WHIZZ_NUMBER))
+            return BUZZ+WHIZZ;
+        if(isMultiple(number, FIZZ_NUMBER) && isMultiple(number, WHIZZ_NUMBER) && isContained(number, BUZZ_NUMBER) && !isContained(number, WHIZZ_NUMBER))
+            return WHIZZ;
+        if(isMultiple(number, FIZZ_NUMBER) && isMultiple(number, BUZZ_NUMBER) && isContained(number, BUZZ_NUMBER) && !isContained(number, WHIZZ_NUMBER))
+            return BUZZ;
+        if(isMultiple(number, FIZZ_NUMBER) && isMultiple(number, WHIZZ_NUMBER) && isContained(number, WHIZZ_NUMBER))
+            return FIZZ+WHIZZ;
+        if(isMultiple(number, FIZZ_NUMBER) && isContained(number, WHIZZ_NUMBER))
+            return FIZZ;
+        if(isMultiple(number, WHIZZ_NUMBER) && isContained(number, WHIZZ_NUMBER))
+            return WHIZZ;
+        if(isMultiple(number, FIZZ_NUMBER) && isMultiple(number, BUZZ_NUMBER) && isMultiple(number, WHIZZ_NUMBER))
+            return FIZZ+BUZZ+WHIZZ;
+        if(isMultiple(number, FIZZ_NUMBER) && isMultiple(number, BUZZ_NUMBER))
+            return FIZZ+BUZZ;
+        if(isMultiple(number, FIZZ_NUMBER) && isMultiple(number, WHIZZ_NUMBER))
+            return FIZZ+WHIZZ;
+        if(isMultiple(number, BUZZ_NUMBER) && isMultiple(number, WHIZZ_NUMBER))
+            return BUZZ+WHIZZ;
+        else if(isMultiple(number, FIZZ_NUMBER))
+            return FIZZ;
+        else if(isMultiple(number, BUZZ_NUMBER))
+            return BUZZ;
+        else if(isMultiple(number, WHIZZ_NUMBER))
+            return WHIZZ;
         return String.valueOf(number);
+    }
+
+    private static boolean isContained(int number, int n)
+    {
+        return String.valueOf(number).contains(String.valueOf(n));
+    }
+
+    private static boolean isMultiple(int number, int n)
+    {
+        return number%n==0;
     }
 }
